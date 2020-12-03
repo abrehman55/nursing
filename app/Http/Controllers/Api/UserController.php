04639144bf9dee411c85e7ac36d1abd1d40de6f9
itriteckodes@gmail.com <itriteckodes@gmail.com>
2020-12-03 14:43:22 +0500

    update

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -24,6 +24,8 @@ class UserController extends Controller
 
         if ($request->qualifications) {
 
+            return $request->qualifications;
+
             foreach ($request->qualifications as $item) {
 
                 $qual = Qualification::where('degree', $item->degree)->where('institude', $item->institude)->first();
