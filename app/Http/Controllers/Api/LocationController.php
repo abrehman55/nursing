6fd620a9c20173c73b21090615bebf20013bd669
itriteckodes@gmail.com <itriteckodes@gmail.com>
2020-12-04 22:09:15 +0500

    update

--- app/Http/Controllers/Api/LocationController.php
+++ app/Http/Controllers/Api/LocationController.php
@@ -32,7 +32,7 @@ class LocationController extends Controller
             return ($a->distance < $b->distance) ? -1 : 1;
         });
 
-        return Api::setResponse('nearby_nurses', $sorted);
+        return Api::setResponse('nearby_nurses', $sorted->all());
     }
     
     public function nearbyHospitals(Request $request){
