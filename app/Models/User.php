5ce656e945a170e779f6224d0f3f5e2d690e5650
Rubina-Aman <rubinaaman2017@gmail.com>
2020-12-30 00:56:37 +0500

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,6 +56,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'verified' => 'boolean',
     ];
 
     public function setPasswordAttribute($value){
