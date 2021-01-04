d876763e274903d2e6a8fcb3b4491cf4c1acf53e
faizan dilbaz <itriteckodes@gmail.com>
2021-01-04 19:39:51 +0500

    update

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -62,7 +62,6 @@ class UserController extends Controller
     public function verify(Request $request){
         $user = User::find($request->user_id);
 
-        return $user;
         if($user->code == $request->code){
             $user->verified = true;
         }
